68c67100475b049c042bc9a9cffe681e062a1468
Rick Grubin <Richard.Grubin@noaa.gov>
2024-11-27 11:17:27 -0600

    WIP

--- modulefiles/ufs_orion.intel.lua
+++ modulefiles/ufs_orion.intel.lua
@@ -6,7 +6,6 @@ loads UFS Model prerequisites for Orion/Intel
 prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/jenkins/workspace/spack-stack/ufs/Orion/weekly/envs/ue-intel/install/modulefiles/Core")
 
 stack_intel_ver=os.getenv("stack_intel_ver") or "2021.9.0"
-io.stderr:write("stack_intel_ver ", stack_intel_ver,"\n")
 load(pathJoin("stack-intel", stack_intel_ver))
 
 stack_impi_ver=os.getenv("stack_impi_ver") or "2021.9.0"
@@ -16,17 +15,6 @@ load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 cmake_ver=os.getenv("cmake_ver") or "3.27.9"
 load(pathJoin("cmake", cmake_ver))
 
--- load("ufs_common")
-
-io.stderr:write("stack_impi_ver ",stack_impi_ver,"\n")
-io.stderr:write("cmake_ver ",cmake_ver,"\n")
-
--- HDF5 needed for LM4
--- hdf5_ver=os.getenv("hdf5_ver") or "1.14.0"
--- hdf5_ver=os.getenv("hdf5_ver") or "1.14.3"
--- load(pathJoin("hdf5", hdf5_ver))
--- nccmp_ver=os.getenv("nccmp_ver") or "1.9.0.1"
--- load(pathJoin("nccmp", nccmp_ver))
 
 load("ufs-weather-model-env/1.0.0")
 load("sp/2.5.0")
