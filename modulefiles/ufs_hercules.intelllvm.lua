0be8b49760d3975228ce997820d6fce5b11ac3ec
Rick Grubin <Richard.Grubin@noaa.gov>
2026-03-17 13:34:12 -0500

    WIP hercules and orion modulefiles update [skip ci]

--- modulefiles/ufs_hercules.intelllvm.lua
+++ modulefiles/ufs_hercules.intelllvm.lua
@@ -1,32 +1,34 @@
 help([[
-loads UFS Model prerequisites for Hercules/IntelLLVM
+loads UFS Model prerequisites for OrionLLVM/Intel
 ]])
 
-prepend_path("MODULEPATH", "/apps/contrib/spack-stack/spack-stack-1.9.2/envs/ue-oneapi-2024.1.0/install/modulefiles/Core")
-prepend_path("MODULEPATH", "/apps/contrib/spack-stack/spack-stack-1.9.2/envs/ue-oneapi-2024.1.0/install/modulefiles/intel-oneapi-mpi/2021.13-sqiixt7/gcc/13.3.0")
+prepend)path("MODULEPATH", "/apps/contrib/spack-stack/modulefiles")
+prepend_path("MODULEPATH", "/apps/contrib/spack-stack/spack-stack-2.1.0/envs/ue-oneapi-2025.3.1/modules/Core")
 
-stack_intel_ver=os.getenv("stack_intel_ver") or "2024.2.1"
+stack_intel_ver=os.getenv("stack_intel_ver") or "2025.3.1"
 load(pathJoin("stack-oneapi", stack_intel_ver))
 
-stack_impi_ver=os.getenv("stack_impi_ver") or "2021.13"
+stack_impi_ver=os.getenv("stack_impi_ver") or "2021.17"
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 
-cmake_ver=os.getenv("cmake_ver") or "3.27.9"
+cmake_ver=os.getenv("cmake_ver") or "3.31.8"
 load(pathJoin("cmake", cmake_ver))
 
 load("ufs_common")
-load("zlib/1.2.13")
+-- load("zlib/1.2.13")
 
 nccmp_ver=os.getenv("nccmp_ver") or "1.9.0.1"
 load(pathJoin("nccmp", nccmp_ver))
+-- tar_ver=os.getenv("tar_ver") or "1.34"
+-- load(pathJoin("tar", tar_ver))
 
 setenv("I_MPI_CC", "icx")
 setenv("I_MPI_CXX", "icpx")
 setenv("I_MPI_F90", "ifx")
 
-setenv("CC", "mpiicc")
-setenv("CXX", "mpiicpc")
-setenv("FC", "mpiifort")
-setenv("CMAKE_Platform", "hercules.intel")
+setenv("CC", "mpiicx")
+setenv("CXX", "mpiicpx")
+setenv("FC", "mpiifx")
+setenv("CMAKE_Platform", "orion.intel")
 
 whatis("Description: UFS build environment")
--- modulefiles/ufs_orion.intelllvm.lua
+++ modulefiles/ufs_orion.intelllvm.lua
@@ -2,29 +2,29 @@ help([[
 loads UFS Model prerequisites for OrionLLVM/Intel
 ]])
 
-prepend_path("MODULEPATH", "/apps/contrib/spack-stack/spack-stack-1.9.2/envs/ue-oneapi-2024.1.0/install/modulefiles/Core")
-prepend_path("MODULEPATH", "/apps/contrib/spack-stack/spack-stack-1.9.2/envs/ue-oneapi-2024.1.0/install/modulefiles/intel-oneapi-mpi/2021.13-li242lf/gcc/12.2.0")
+prepend)path("MODULEPATH", "/apps/contrib/spack-stack/modulefiles")
+prepend_path("MODULEPATH", "/apps/contrib/spack-stack/spack-stack-2.1.0/envs/ue-oneapi-2025.3.1/modules/Core")
 
-stack_intel_ver=os.getenv("stack_intel_ver") or "2024.2.1"
+stack_intel_ver=os.getenv("stack_intel_ver") or "2025.3.1"
 load(pathJoin("stack-oneapi", stack_intel_ver))
 
-stack_impi_ver=os.getenv("stack_impi_ver") or "2021.13"
+stack_impi_ver=os.getenv("stack_impi_ver") or "2021.17"
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 
-cmake_ver=os.getenv("cmake_ver") or "3.27.9"
+cmake_ver=os.getenv("cmake_ver") or "3.31.8"
 load(pathJoin("cmake", cmake_ver))
 
 load("ufs_common")
-load("zlib/1.2.13")
+-- load("zlib/1.2.13")
 
 nccmp_ver=os.getenv("nccmp_ver") or "1.9.0.1"
 load(pathJoin("nccmp", nccmp_ver))
-tar_ver=os.getenv("tar_ver") or "1.34"
-load(pathJoin("tar", tar_ver))
+-- tar_ver=os.getenv("tar_ver") or "1.34"
+-- load(pathJoin("tar", tar_ver))
 
 setenv("I_MPI_CC", "icx")
 setenv("I_MPI_CXX", "icpx")
-setenv("I_MPI_F90", "ifort")
+setenv("I_MPI_F90", "ifx")
 
 setenv("CC", "mpiicx")
 setenv("CXX", "mpiicpx")
