2529e00e71da6f2b4b08fc8af8a9b6b74c7c067f
Rick Grubin <Richard.Grubin@noaa.gov>
2026-03-17 16:07:42 -0500

    fix lua file typo [skip ci]

--- modulefiles/ufs_orion.intelllvm.lua
+++ modulefiles/ufs_orion.intelllvm.lua
@@ -2,7 +2,7 @@ help([[
 loads UFS Model prerequisites for OrionLLVM/Intel
 ]])
 
-prepend)path("MODULEPATH", "/apps/contrib/spack-stack/modulefiles")
+prepend_path("MODULEPATH", "/apps/contrib/spack-stack/modulefiles")
 prepend_path("MODULEPATH", "/apps/contrib/spack-stack/spack-stack-2.1.0/envs/ue-oneapi-2025.3.1/modules/Core")
 
 stack_intel_ver=os.getenv("stack_intel_ver") or "2025.3.1"
