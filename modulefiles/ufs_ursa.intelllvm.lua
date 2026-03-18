053f3d67a53b3b183fdcfbcd64f46e65074c74ab
Rick Grubin <richard.grubin@noaa.gov>
2026-03-18 22:16:55 +0000

    ursa intelllvm modulefile [skip ci]

--- modulefiles/ufs_ursa.intelllvm.lua
+++ modulefiles/ufs_ursa.intelllvm.lua
@@ -2,16 +2,15 @@ help([[
 loads UFS Model prerequisites for Ursa/Intel LLVM
 ]])
 
-prepend_path("MODULEPATH", "/contrib/spack-stack/spack-stack-1.9.2/envs/ue-oneapi-2024.2.1/install/modulefiles/Core")
-prepend_path("MODULEPATH", "/contrib/spack-stack/spack-stack-1.9.2/envs/ue-oneapi-2024.2.1/install/modulefiles/intel-oneapi-mpi/2021.13-haww6b3/gcc/12.4.0")
+prepend_path("MODULEPATH", "b/spack-stack/spack-stack-2.1.0/envs/ue-oneapi-2025.3.1/modules/Core")
 
-stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2024.2.1"
+stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2025.3.1"
 load(pathJoin("stack-oneapi", stack_oneapi_ver))
 
-stack_impi_ver=os.getenv("stack_impi_ver") or "2021.13"
+stack_impi_ver=os.getenv("stack_impi_ver") or "2021.17"
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 
-cmake_ver=os.getenv("cmake_ver") or "3.27.9"
+cmake_ver=os.getenv("cmake_ver") or "3.31.8"
 load(pathJoin("cmake", cmake_ver))
 
 load("ufs_common")
