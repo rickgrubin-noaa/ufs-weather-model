2ac314be82c9c228cb1e43609d04cac148f23b72
Rick Grubin <richard.grubin@noaa.gov>
2026-03-18 22:25:22 +0000

    fix typo ursa intelllvm modulefile [skip ci]

--- modulefiles/ufs_ursa.intelllvm.lua
+++ modulefiles/ufs_ursa.intelllvm.lua
@@ -5,7 +5,7 @@ loads UFS Model prerequisites for Ursa/Intel LLVM
 prepend_path("MODULEPATH", "/contrib/spack-stack/spack-stack-2.1.0/envs/ue-oneapi-2025.3.1/modules/Core")
 
 stack_oneapi_ver=os.getenv("stack_oneapi_ver") or "2025.3.1"
-load(pathJoin("stack-oneapi", stack_oneapi_ver))
+load(pathJoin("stack-intel-oneapi-compilers", stack_oneapi_ver))
 
 stack_impi_ver=os.getenv("stack_impi_ver") or "2021.17"
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
